3a882b2d608c31dbe58d19acdc9bfbd1978d60b4
Victor Matheus <victormatheus507@gmail.com>
2024-01-17 21:37:50 -0300

    props 1

---
+++ tsx-basico/props/src/App.tsx
@@ -0,0 +1,20 @@
+import React from "react"
+import Button from "./Button"
+
+function App() {
+
+  const [total,setTotal] = React.useState(0)
+
+  function incrementar() {
+    setTotal((total) => total + 1)
+  }
+
+  return (
+    <div>
+      <p>Total: {total}</p> 
+      <Button onClick={incrementar} tamanho='1.25rem'>Incrementar 2</Button>
+    </div>
+  )
+}
+
+export default App
